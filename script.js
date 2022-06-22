66424c332f2fd9859111ab40b92ec5ff8ee91336
Leowilson17 <104545949+Leowilson17@users.noreply.github.com>
2022-06-22 09:52:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var year = parseInt(prompt("Enter Year"));
 if ( year % 4 == 0)
 {
-    console.log("This is lear Year");
+    console.log("This is leap Year");
 }
 else{
     console.log("Not a Leap Year");
